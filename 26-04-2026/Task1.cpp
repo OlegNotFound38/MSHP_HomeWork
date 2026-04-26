55487db4bb8d746ce159f397dfa17c6c26f3df6e
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-26 12:53:31 +0300

    Я тупой, добавил оболочку как в задании

--- 26-04-2026/Task1.cpp
+++ 26-04-2026/Task1.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
 using namespace std;
+int main() {
+    int n, m;
+    int arr[100][100];
+    cin >> n >> m;
 
-int main(){
+// вставляемый код ниже
 
+    
+
+// вставляемый код выше
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
