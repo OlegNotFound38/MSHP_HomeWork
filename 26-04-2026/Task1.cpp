db53a2d20829b9ae627686e8ccd9ad203c552126
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-26 12:57:58 +0300

    работает

--- 26-04-2026/Task1.cpp
+++ 26-04-2026/Task1.cpp
@@ -4,10 +4,13 @@ int main() {
     int n, m;
     int arr[100][100];
     cin >> n >> m;
-
 // вставляемый код ниже
 
-    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            arr[i][j] = j;
+        }
+    }
 
 // вставляемый код выше
     for (int i = 0; i < n; i++) {
