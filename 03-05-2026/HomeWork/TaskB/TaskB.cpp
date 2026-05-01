3bc992a2c2ae4a3f791a93cf4e8c48681172597c
OlegNotFound38 <pronkinoa@gmail.com>
2026-05-01 12:05:58 +0300

    вывод суммы

--- 03-05-2026/HomeWork/TaskB/TaskB.cpp
+++ 03-05-2026/HomeWork/TaskB/TaskB.cpp
@@ -10,7 +10,13 @@ int main() {
         }
     }
 
-    
+    int column_sum = 0;
+    for (int j = 0; j < M; j++){
+        column_sum = 0;
+        for (int i = 0; i < N; i++){column_sum += arr[i][j];}
+
+        cout << column_sum << " ";
+    }
 
     return 0;
 }
