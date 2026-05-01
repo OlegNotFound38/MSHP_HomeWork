c09b814aa9bbdc459fb63034165b2ecb2d2fe8ad
OlegNotFound38 <pronkinoa@gmail.com>
2026-05-01 11:42:16 +0300

    ввод матрицы

--- 03-05-2026/HomeWork/TaskA/TaskA.cpp
+++ 03-05-2026/HomeWork/TaskA/TaskA.cpp
@@ -4,20 +4,12 @@ int main() {
     int n, m;
     int arr[100][100];
     cin >> n >> m;
-// вставляемый код ниже
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
-            arr[i][j] = j;
+            cin >> arr[i][j];
         }
     }
-
-// вставляемый код выше
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
+    
     return 0;
 }
