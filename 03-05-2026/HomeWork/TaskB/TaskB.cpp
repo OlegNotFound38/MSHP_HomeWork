0129173701a1204cdbd898eb4d569e5063bf0bda
OlegNotFound38 <pronkinoa@gmail.com>
2026-05-01 12:01:29 +0300

    ввод массива

--- 03-05-2026/HomeWork/TaskB/TaskB.cpp
+++ 03-05-2026/HomeWork/TaskB/TaskB.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int N;
-    int arr[500][500];
-    cin >> N;
+    int arr[100][100],
+    N, M; cin >> N >> M;
 
     for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
+        for (int j = 0; j < M; j++){
             cin >> arr[i][j];
         }
     }
 
-    for (int i = 0; i < N; i++){
-        cout << arr[i][N-1-i] << " ";
-    }
+    
 
     return 0;
 }
