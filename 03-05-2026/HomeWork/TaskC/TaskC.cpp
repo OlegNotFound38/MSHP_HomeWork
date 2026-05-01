62aa9228aa82fc249182bfa3cf954da8b45fe0b2
OlegNotFound38 <pronkinoa@gmail.com>
2026-05-01 12:08:18 +0300

    ввод массива

--- 03-05-2026/HomeWork/TaskC/TaskC.cpp
+++ 03-05-2026/HomeWork/TaskC/TaskC.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int N;
-    int arr[500][500];
-    cin >> N;
+    int arr[100][100], 
+    n, m; cin >> n >> m;
 
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
             cin >> arr[i][j];
         }
     }
-
-    int min = arr[1][1];
-    for (int i = 0; i < N; i++){
-        if (arr[i][i] < min){min = arr[i][i];}
-    }
-
-    cout << min;
 
     return 0;
 }
