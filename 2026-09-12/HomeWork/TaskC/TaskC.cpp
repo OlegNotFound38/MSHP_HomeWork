55e32173f9747e0027bba2feee8fe3ed47b21f16
OlegNotFound38 <pronkinoa@gmail.com>
2026-09-12 10:20:10 +0300

    fixed and optimize

--- 2026-09-12/HomeWork/TaskC/TaskC.cpp
+++ 2026-09-12/HomeWork/TaskC/TaskC.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main (){
     int a, b; cin >> a >> b;
 
-    for (int i = a; i <= b; i++){
-        for (int j = 1; j < i; j++){
-            if (i / j == j && i % j == 0) cout << i << ' ';
-        }
+    for (int i = 1; i*i <= b; i++){
+        if (i*i >= a) cout << i*i << " "; // && i*i <= b)
     }
 
     return 0;
